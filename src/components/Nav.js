a2f2f545c29c1089967570d6a9ec70b1e39594ca
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-10-08 14:19:21 +0200

    Add dummy navigation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../assets/arrow-left.svg';
+import '../Style/Navigation.css';
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <nav>
+      <div className="navigation">
+        <img src={logo} alt="back" />
+        <span>Back</span>
+      </div>
+
+      <ul className="Items">
+        <li>
+          <NavLink to="/air-details" activeClassName="active">
+            Air Details
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
