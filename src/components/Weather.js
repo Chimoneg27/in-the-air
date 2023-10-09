2c08983eb2a0cb8b5041892eba7a9596f9b22e10
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-10-09 10:32:18 +0200

    Udpate weather files

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { citiesWeather } from '../redux/weatherSlice/weatherSlice';
+import Nav from './Nav';
+import Capitals from './Capitals';
+import arrow from '../assets/arrow-right.svg';
+import '../Style/Capitals.css';
 
-function Weather() {
+const Weather = () => {
+  const capitalData = useSelector((state) => state.weather.cityData);
+  const isDataFetched = useSelector((state) => state.weather.isDataFetched);
+  const dispatch = useDispatch();
+
+  const [filteredCities, setFilteredCities] = useState([]);
+  const handleFilterChange = (filterText) => {
+    const filtered = capitalData.filter(
+      (city) => city.city.toLowerCase().includes(filterText.toLowerCase()),
+    );
+    setFilteredCities(filtered);
+  };
+
+  useEffect(() => {
+    if (isDataFetched === false) {
+      dispatch(citiesWeather());
+    }
+  }, [dispatch, isDataFetched]);
+
+  useEffect(() => {
+    setFilteredCities(capitalData);
+  }, [capitalData]);
+
   return (
-    <div>Weather</div>
+    <div id="main-wrapper">
+      <Nav handleFilterChange={handleFilterChange} />
+      <div className="cities">
+        {filteredCities.map((city) => (
+          <Link className="flex" to={`city/${city.id}`} key={city.id}>
+            <div className="image">
+              <img className="arrow" src={arrow} alt="right-arrow" />
+            </div>
+            <Capitals
+              city={city.city}
+              aqi={city.data.main.aqi}
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
   );
-}
+};
 
 export default Weather;
--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
+import { useParams, Link } from 'react-router-dom';
+import AirDetails from './AirDetails';
+import AirDetailed from './AirDetailed';
 
-function WeatherInfo() {
+const WeatherInfo = () => {
+  const cityData = useSelector((state) => state.weather.cityData);
+  const { id } = useParams();
+  const selectCapitalData = cityData.find((city) => city.id === id);
+  const cityName = selectCapitalData.city ? selectCapitalData.city : '';
+  const air = AirDetails(id, cityData);
+
   return (
-    <div>WeatherInfo</div>
+    <div className="air-details">
+      <nav className="details-nav">
+        <Link to="/" className="back">
+          <span className="arrow">←</span>
+          Back
+        </Link>
+        <h2>{cityName}</h2>
+      </nav>
+      <header className="intro">
+        <h1 className="title">Air Quality</h1>
+      </header>
+      <section className="conc-header">Concentration in μg/m3</section>
+      <section className="pollution-data">
+        {air.map((component) => (
+          <AirDetailed
+            key={component.dataname}
+            dataname={component.dataname}
+            data={component.data}
+          />
+        ))}
+      </section>
+    </div>
   );
-}
+};
 
 export default WeatherInfo;
