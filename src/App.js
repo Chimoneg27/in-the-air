930c381ccd7892a2477e3f2d79acca1aacf30945
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-10-10 17:35:38 +0200

    Remove browser router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Weather from './components/Weather';
 import WeatherInfo from './components/WeatherInfo';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Weather />} />
-        <Route path="/city/:id" element={<WeatherInfo />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Weather />} />
+      <Route path="/city/:id" element={<WeatherInfo />} />
+    </Routes>
   );
 }
 
